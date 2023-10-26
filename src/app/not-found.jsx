85c6ea442c24c375170323af7cd031e6e9591908
ruanJS <rm551096@fiap.com.br>
2023-10-25 23:26:33 -0300

    arrumando o not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="bg-red-400 font-extrabold font-sans m-auto w-96">
-        <h1 className="text-center text-4xl">!OOPS - 404</h1>
-        <p>!Not Found</p>
-        <figure>
-            <Image src="https://httpstatusdogs.com/img/404.jpg" alt="404" width={650} height={500}/>
-        </figure>
+    <div className="bg-red-400 text-black font-extrabold text-center font-sans w-96 mx-auto p-8">
+      <h1 className="text-4xl">OOPS - 404</h1>
+      <p>Not Found</p>
+      <figure>
+        <Image src="https://httpstatusdogs.com/img/404.jpg" alt="404" width={650} height={500} />
+      </figure>
     </div>
-  )
+  );
 }
